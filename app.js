265be914256c1a84be90728a4d50b251b0d07e82
varinea <varinea.lopez@gmail.com>
2020-03-09 07:27:33 -0300

    agregando diseño y otras apis

--- app.js
+++ app.js
@@ -48,7 +48,9 @@ function displayBounds(firstMap, secondMap) {
 // initialize communication with the platform
 // In your own code, replace variable window.apikey with your own apikey
 var platform = new H.service.Platform({
-  apikey: 'kgSdjqerT-AJ-VhQvIwifffAhb9AuVu7LK3gMkbIweA'
+  apikey: 'kgSdjqerT-AJ-VhQvIwifffAhb9AuVu7LK3gMkbIweA',
+  useHTTPS: true,
+  useCIT: true
 });
 // create two sets of the default layers for each map instance
 var defaultLayers = platform.createDefaultLayers();
@@ -195,8 +197,8 @@ containerNode.className = 'btn-group ';
 
 // Create the "Capture" button
 var captureBtn = document.createElement('input');
-captureBtn.value = 'Captura';
-captureBtn.type = 'button';
+captureBtn.value = 'Tomar Captura';
+captureBtn.type = '<br><br><br> button';
 captureBtn.className = 'btn btn-primary';
 
 // Add both button and container to the DOM
@@ -207,3 +209,90 @@ mapContainer.appendChild(containerNode);
 captureBtn.onclick = function () {
   capture(resultContainer, map, ui);
 }; 
+
+
+//Contexto centrar el mapa con el click derecho
+
+
+function addContextMenus(map) {
+  // First we need to subscribe to the "contextmenu" event on the map
+  map.addEventListener('contextmenu', function (e) {
+    // As we already handle contextmenu event callback on circle object,
+    // we don't do anything if target is different than the map.
+    if (e.target !== map) {
+      return;
+    }
+
+    // "contextmenu" event might be triggered not only by a pointer,
+    // but a keyboard button as well. That's why ContextMenuEvent
+    // doesn't have a "currentPointer" property.
+    // Instead it has "viewportX" and "viewportY" properties
+    // for the associates position.
+
+    // Get geo coordinates from the screen coordinates.
+    var coord  = map.screenToGeo(e.viewportX, e.viewportY);
+
+    // In order to add menu items, you have to push them to the "items"
+    // property of the event object. That has to be done synchronously, otherwise
+    // the ui component will not contain them. However you can change the menu entry
+    // text asynchronously.
+    e.items.push(
+      // Create a menu item, that has only a label,
+      // which displays the current coordinates.
+      new H.util.ContextItem({
+        label: [
+          Math.abs(coord.lat.toFixed(4)) + ((coord.lat > 0) ? 'N' : 'S'),
+          Math.abs(coord.lng.toFixed(4)) + ((coord.lng > 0) ? 'E' : 'W')
+        ].join(' ')
+      }),
+      // Create an item, that will change the map center when clicking on it.
+      new H.util.ContextItem({
+        label: 'Centrar el mapa aqui',
+        callback: function() {
+          map.setCenter(coord, true);
+        }
+      }),
+      // It is possible to add a seperator between items in order to logically group them.
+      H.util.ContextItem.SEPARATOR,
+      // This menu item will add a new circle to the map
+      new H.util.ContextItem({
+        label: 'Agregar Circulo',
+        callback: addCircle.bind(map, coord)
+      })
+    );
+  });
+}
+
+/**
+ * Adds a circle which has a context menu item to remove itself.
+ *
+ * @this H.Map
+ * @param {H.geo.Point} coord Circle center coordinates
+ */
+function addCircle(coord) {
+  // Create a new circle object
+  var circle = new H.map.Circle(coord, 5000),
+      map = this;
+
+  // Subscribe to the "contextmenu" eventas we did for the map.
+  circle.addEventListener('contextmenu', function(e) {
+    // Add another menu item,
+    // that will be visible only when clicking on this object.
+    //
+    // New item doesn't replace items, which are added by the map.
+    // So we may want to add a separator to between them.
+    e.items.push(
+      new H.util.ContextItem({
+        label: 'Remove',
+        callback: function() {
+          map.removeObject(circle);
+        }
+      })
+    );
+  });
+
+  // Make the circle visible, by adding it to the map
+  map.addObject(circle);
+}
+
+addContextMenus(map);
